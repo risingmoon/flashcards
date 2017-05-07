f9e24879b49e5891902b80ba43c194da2be46bac
Justin Lee <justindavidlee88@gmail.com>
2017-05-06 23:53:33 -0700

    First simple flashcard

--- flashcards/ViewController.swift
+++ flashcards/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var words = [String]()
+    var translations = [String]()
+    var size = 0
+    var number = 0
+
+    //MARK: Properties
+    @IBOutlet weak var label: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let bundle = Bundle.main
+        let path = bundle.path(forResource: "sample", ofType: "csv")
+        let content = try! String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+        let rows = content.components(separatedBy: "\n")
+        size = rows.count - 1
+        for row in rows {
+            let columns = row.components(separatedBy: ",")
+            //label.text = String(columns.count)
+            if columns.count > 1 {
+                words.append(columns[0])
+                translations.append(columns[1])
+            }
+        }
+        label.text = words[number]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +42,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: Actions
+    @IBAction func translate(_ sender: UIButton) {
+        label.text = translations[number]
+    }
+    @IBAction func next(_ sender: UIButton) {
+        number = (number + 1) % size
+        label.text = words[number]
+    }
 }
 
